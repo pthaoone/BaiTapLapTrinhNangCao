e5a32cb66267a3fda0b14b86061c0b31a63ccd16
thaonee <phuongthao23826@gmail.com>
2026-09-12 10:21:50 +0700

    First commit

---
+++ BaiTap/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b, c;
+
+        // Enter number 1
+        while (true)
+        {
+            Console.Write("Nhap so thu nhat: ");
+            try
+            {
+                a = int.Parse(Console.ReadLine());
+                break;
+            }
+            catch
+            {
+                Console.WriteLine("Du lieu khong hop le! Vui long nhap lai.");
+            }
+        }
+
+        // Enter number 2
+        while (true)
+        {
+            Console.Write("Nhap so thu hai: ");
+            try
+            {
+                b = int.Parse(Console.ReadLine());
+                break;
+            }
+            catch
+            {
+                Console.WriteLine("Du lieu khong hop le! Vui long nhap lai.");
+            }
+        }
+
+        // Enter number 3
+        while (true)
+        {
+            Console.Write("Nhap so thu ba: ");
+            try
+            {
+                c = int.Parse(Console.ReadLine());
+                break;
+            }
+            catch
+            {
+                Console.WriteLine("Du lieu khong hop le! Vui long nhap lai.");
+            }
+        }
+
+        // Caculate sum
+        Console.WriteLine($"{a} + {b} + {c} = {a+b+c}");
+    }
+}
